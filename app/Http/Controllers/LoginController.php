f1c5fc55d4b7b5df39cf1ea77efe6ceb40de4402
Raúl Pacheco Ropero <127249179+RaulPachecoo@users.noreply.github.com>
2025-06-03 11:25:11 +0200

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,9 @@ class LoginController extends Controller
 {
     public function index()
     {
+        if (\Illuminate\Support\Facades\Auth::check()) {
+            return redirect()->route('home');
+        }
         return view('auth.login');
     }
 
